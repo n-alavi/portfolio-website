e1ee98507c04f0e6f0d5c2f7425b3537f00c0e65
nargess alavi <alavi_n70@yahoo.com>
2024-09-29 22:56:10 +0330

    improve style

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
       >
         {/* gradient background effect */}
 
-        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25 rem] w-[31.25rem] ounded-full bg-[#fbe2e3] blur-[10rem] dark:bg-[#946263] sm:w-[68.75rem]"></div>
-        <div className="2xl:left-[-5rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fd] blur-[10rem] dark:bg-[#676394] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem]"></div>
+        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25 rem] w-[31.25rem] ounded-full bg-[#fbe2e3] blur-[10rem]  sm:w-[68.75rem]"></div>
+        <div className="2xl:left-[-5rem] absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#dbd7fd] blur-[10rem]  sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem]"></div>
         {children}
       </body>
     </html>
