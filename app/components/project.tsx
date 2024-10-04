a0b6a3c50007ae30d1b6db47a0f584f7a37e3c2b
nargess alavi <alavi_n70@yahoo.com>
2024-10-04 23:45:25 +0330

    fix: add div with id to ensure correct page anchor link behavior

--- app/components/project.tsx
+++ app/components/project.tsx
@@ -10,19 +10,21 @@ export default function Projects() {
   }); // Use the hook
 
   return (
-    <motion.div
-      className="mb-28 mt-20 w-full max-w-[45rem] scroll-mt-28 px-4 sm:mb-40"
-      initial={{ opacity: 0, y: 100 }}
-      animate={controls}
-      transition={{ duration: 0.8, ease: "easeOut" }} // Adjust timing and easing
-      ref={ref} // Attach the ref to the section to observe
-      id="projects"
-    >
-      <>
-        <h1 className="text-3xl font-bold text-center mb-8">پروژه های من</h1>
+    <div id="projects">
+      <motion.div
+        className="mb-28 mt-20 w-full max-w-[45rem] scroll-mt-28 px-4 sm:mb-40"
+        initial={{ opacity: 0, y: 100 }}
+        animate={controls}
+        transition={{ duration: 0.8, ease: "easeOut" }} // Adjust timing and easing
+        ref={ref} // Attach the ref to the section to observe
+        id="projects"
+      >
+        <>
+          <h1 className="text-3xl font-bold text-center mb-8">پروژه های من</h1>
 
-        <h1 className="text-2xl font-bold my-24">به زودی... </h1>
-      </>
-    </motion.div>
+          <h1 className="text-2xl font-bold my-24">به زودی... </h1>
+        </>
+      </motion.div>
+    </div>
   );
 }
--- app/components/skillcards.tsx
+++ app/components/skillcards.tsx
@@ -11,35 +11,37 @@ export default function SkillsCard() {
   }); // Use the hook
 
   return (
-    <motion.section
-      className="mb-28 mt-20 w-full max-w-[45rem] scroll-mt-28 px-4 sm:mb-40"
-      initial={{ opacity: 0, y: 100 }}
-      animate={controls}
-      transition={{ duration: 0.8, ease: "easeOut" }} // Adjust timing and easing
-      ref={ref} // Attach the ref to the section to observe
-      id="skills"
-    >
-      <div className=" px-4 max-w-5xl mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8">مهارت ها </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {skills.map((skill, index) => (
-            <div
-              key={index}
-              className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-            >
-              <div className="flex items-center justify-center mb-4">
-                {skill.icon}
+    <div id="skills">
+      <motion.section
+        className="mb-28 mt-20 w-full max-w-[45rem] scroll-mt-28 px-4 sm:mb-40"
+        initial={{ opacity: 0, y: 100 }}
+        animate={controls}
+        transition={{ duration: 0.8, ease: "easeOut" }} // Adjust timing and easing
+        ref={ref} // Attach the ref to the section to observe
+        id="skills"
+      >
+        <div className=" px-4 max-w-5xl mx-auto">
+          <h2 className="text-3xl font-bold text-center mb-8">مهارت ها </h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+            {skills.map((skill, index) => (
+              <div
+                key={index}
+                className="p-6 bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+              >
+                <div className="flex items-center justify-center mb-4">
+                  {skill.icon}
+                </div>
+                <h3 className="text-xl font-semibold text-gray-800 text-center">
+                  {skill.name}
+                </h3>
+                <p className="text-gray-600 mt-2 text-center">
+                  {skill.description}
+                </p>
               </div>
-              <h3 className="text-xl font-semibold text-gray-800 text-center">
-                {skill.name}
-              </h3>
-              <p className="text-gray-600 mt-2 text-center">
-                {skill.description}
-              </p>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </motion.section>
+      </motion.section>
+    </div>
   );
 }
