7f4a04a1f35648fe592b01a64771bff600121cd1
nargess alavi <alavi_n70@yahoo.com>
2024-10-10 00:21:19 +0330

    feat(auth): add session management for authentication on protected page

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,34 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+import UserCard from "../components/card/usercard";
+
 export default function Dashboard() {
-<<<<<<< HEAD
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      redirect("/api/auth/signin?callbackUrl=/quiz");
+    },
+  });
+
   return (
     <div>
-      <h3>this page is dashboard</h3>
+      {session?.user ? (
+        <>
+          <UserCard user={session?.user} />
+          <div className="w-full mt-25 p-4 text-center">
+            <Link
+              href="/api/auth/signout"
+              className="mx-1 my-5 px-6 py-1 text-sm rounded shadow bg-red-600 hover:bg-red-400 text-red-200 w-20 cursor-pointer"
+            >
+              خروج
+            </Link>
+          </div>
+        </>
+      ) : null}
+      <h3>this is dashboard</h3>
     </div>
   );
-=======
-  return <h3>this is dashboard</h3>;
->>>>>>> 46834babe7d773998ba83d34b86efcf4ce0356d3
 }
